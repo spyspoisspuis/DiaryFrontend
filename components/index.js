75b821f0306a5379afb01ab9832b4a6b4f02255f
Waris Lakthong <77393871+spyspoisspuis@users.noreply.github.com>
2023-07-25 16:30:58 +0700

    Export component

--- components/index.js
+++ components/index.js
@@ -1,4 +1,8 @@
 export {default as Header} from './Header/Header';
 export {default as LoginWebHeader} from './LoginHeader/LoginHeader';
-export {default as Clock} from './Clock/Clock';
-
+export {default as SelectUserNote} from './SelectUserNote/SelectUserNote';
+export {default as SelectUserNoteTab} from './SelectUserNote/SelectUserNoteTab';
+export {default as SelectUserNoteDropdown} from './SelectUserNote/SelectUserNoteDropdown';
+export {default as Clock} from './Clock/Clock'
+export {default as Diary} from './Diary/Diary'
+export {default as AddDiaryForm} from './AddDiaryForm/AddDiaryForm'
